e2586aa6fd5c5981cd831b533b9e93af0e352e42
ismaildrs <drissiisismail@gmail.com>
2025-03-24 16:40:46 +0000

    feat: persistent storage

--- components/CheckboxInput.js
+++ components/CheckboxInput.js
@@ -1,145 +1,150 @@
-class CheckboxInput extends HTMLElement {
+class RangeInput extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
 
-    const checked = this.getAttribute("checked");
-    const label = this.getAttribute("label");
+    this.loadSettingsData();
 
     this.shadowRoot.innerHTML = `
-    <style>
-      .container {
-        display: block;
-        position: relative;
-        height: 20px;
-        padding-left: 25px;
-        cursor: pointer;
-        font-size: 14px;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-      
-      /* Hide the browser's default checkbox */
-      .container input {
-        position: absolute;
-        opacity: 0;
-        cursor: pointer;
-        height: 0;
-        width: 0;
-      }
-      
-      /* Create a custom checkbox */
-      .checkmark {
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 20px;
-        width: 20px;
-        background-color: #4a5d79;
-        border-radius: 5px;
-      }
-      
-      /* On mouse-over, add a grey background color */
-      .container:hover input ~ .checkmark {
-        background-color: #556c8b;
-      }
-      
-      /* When the checkbox is checked, add a blue background */
-      .container input:checked ~ .checkmark {
-        background-color: var(--color-current-session);
-      }
-      
-      /* Create the checkmark/indicator (hidden when not checked) */
-      .checkmark:after {
-        content: "";
-        position: absolute;
-        display: none;
-      }
-      
-      /* Show the checkmark when checked */
-      .container input:checked ~ .checkmark:after {
-        display: block;
-      }
-      
-      /* Style the checkmark/indicator */
-      .container .checkmark:after {
-        left: 6px;
-        top: 2px;
-        width: 5px;
-        height: 10px;
-        border: solid white;
-        border-width: 0 3px 3px 0;
-        -webkit-transform: rotate(45deg);
-        -ms-transform: rotate(45deg);
-        transform: rotate(45deg) scale(1);
-      }
-    </style>
+      <style>
+        .settings-slider-wrapper {
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          margin-bottom: 1rem;
+        }
+        
+        .settings-slider-info-wrapper {
+          display: flex;
+          justify-content: space-between;
+          margin-bottom: 0.5rem;
+          width: 100%;
+          font-size: 14px;
+        }
+        
+        input {
+          margin-left: -2px;
+          width: 100%;
+          height: 5px;
+          background: transparent;
+          outline: none;
+          cursor: pointer;
+          border-radius: 10px;
+        }
 
-    <label class="container">
-      <input type="checkbox">
-      <span class="checkmark"></span>
-      ${label}
-    </label>`;
+        input::-moz-range-track,
+        input::-moz-range-progress {
+          width: 100%;
+          height: 100%;
+          border-radius: 10px;
+          background: #4a5d79;
+        }
 
-    const checkbox = this.shadowRoot.querySelector("input");
-    if (checked == "true") checkbox.setAttribute("checked", "true");
-    else checkbox.removeAttribute("checked");
+        input::-moz-range-progress {
+          background: var(--color-current-session);
+        }
 
-    checkbox.addEventListener("click", async () => {
-      const checked = checkbox.getAttribute("checked");
-      if (checked == "true") checkbox.removeAttribute("checked");
-      else checkbox.setAttribute("checked", "true");
+        input::-moz-range-thumb {
+          appearance: none;
+          margin: 0;
+          height: 12px;
+          width: 12px;
+          background: var(--color-current-session);
+          border-radius: 100%;
+          border: 0;
+        }
+        
+        </style>
+    
+      <div class="settings-slider-wrapper">
+        <div class="settings-slider-info-wrapper">
+          <span name="inputInfoName">${this.name}</span>
+          <span name="inputInfoValue"></span>
+        </div>
+        <input type="range" name="sessionLength" min="0" max="${this.max}" step="${this.step}">
+      </div>`;
 
-      this.dispatchEvent(
-        new CustomEvent("checkbox-input", {
-          bubbles: true,
-          composed: true,
-          detail: { value: checkbox.getAttribute("checked") },
-        })
-      );
+    const input = this.shadowRoot.querySelector("input");
+
+    this.setSettingsSlider();
+    input.addEventListener("input", async (event) => {
+      this.updateSettingsSlider();
+
+      // Update SETTINGS 
+      const val = minToMilliseconds(event.currentTarget.value);
+      this.type === "mins"
+        ? (SETTINGS.sessionLength[this.key] = val)
+        : (SETTINGS[this.key] = event.currentTarget.value);
+      sendMessage("update_settings", SETTINGS);
+
+      localStorage.setItem(sessionsToLocalStorage[this.id], event.currentTarget.value);
     });
+
   }
 
-  updateComponent() {}
+  // Added: Loading all variables in one place
+  loadSettingsData() {
+    this.id = this.getAttribute("id");
+    this.key = this.getAttribute("key");
+    this.name = this.getAttribute("name");
+    this.step = this.getAttribute("step") || 1;
+    this.type = this.getAttribute("type") || "mins";
+    this.value = this.getAttribute("value") || (this.type === "mins") ? SETTINGS.sessionLength[this.key] : SETTINGS.sessionRounds;
+    this.max = this.getAttribute("max") || 100;
+  }
+
+  // Updated: update slider settings, when the component is already loaded
+  updateSettingsSlider() {
+    const inputInfoValue = this.shadowRoot.querySelector(
+      'span[name="inputInfoValue"]'
+    );
+
+    const slider = this.shadowRoot.querySelector("input");
+    const sliderValue = Math.max(slider.value, 1);
+
+    if (this.type === "mins") {
+      const value = sliderValue * 60 * 1000 + TIMER_PADDING;
+      inputInfoValue.innerText = durationToString(value, { isVerbose: true });
+    } else {
+      const innerText = `${sliderValue} round${sliderValue > 1 ? "s" : ""}`;
+      inputInfoValue.innerText = innerText;
+    }
+  }
+
+  // Added: update slider settings when the component is loading 
+  setSettingsSlider() {
+    const inputInfoValue = this.shadowRoot.querySelector(
+      'span[name="inputInfoValue"]'
+    );
+
+    const slider = this.shadowRoot.querySelector("input");
+    slider.step = this.step;
+
+    if (this.type === "mins") {
+      const sliderValue = Math.max(SETTINGS.sessionLength[this.key], 1);
+      slider.value = millisecondToMinutes(sliderValue);
+      inputInfoValue.innerText = durationToString(sliderValue, { isVerbose: true });
+    } else {
+      const sliderValue = Math.max(SETTINGS.sessionRounds, 1);
+      slider.value = sliderValue;
+      const innerText = `${sliderValue} round${sliderValue > 1 ? "s" : ""}`;
+      inputInfoValue.innerText = innerText;
+    }
+  }
 
   static get observedAttributes() {
-    return ["checked"];
+    return ["step", "value"];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (name == "checked") {
-      const checkbox = this.shadowRoot.querySelector("input");
-      if (newValue == "true") checkbox.setAttribute("checked", "true");
-      else checkbox.removeAttribute("checked");
+    if (name === "value") {
+      const slider = this.shadowRoot.querySelector("input");
+      slider.value = newValue;
     }
-    if (oldValue !== newValue) this.updateComponent();
+    if (oldValue !== newValue) this.updateSettingsSlider();
   }
 }
 
-customElements.define("checkbox-input", CheckboxInput);
+customElements.define("range-input", RangeInput);
 
-// <style>
-// input {
-//   -ms-transform: scale(1.4);
-//   -moz-transform: scale(1.4);
-//   -webkit-transform: scale(1.4);
-//   -o-transform: scale(1.4);
-//   transform: scale(1.4);
-//   padding: 10px;
-//   accent-color: var(--color-current-session);
-//   outline: none;
-//   cursor: pointer;
-// }
 
-// label {
-//   cursor: pointer;
-//   display: flex;
-//   flex-direction: row;
-//   align-items: center;
-//   gap: 5px;
-//   font-size: 14px;
-// }
-
-// </style>
